6271228fca87943f80fb661878df8139f459a4a2
Nst Gorbenko <nstgorbenko@yandex.ru>
2019-06-17 15:20:21 +0300

    Рефакторинг

--- js/setup.js
+++ js/setup.js
@@ -1,26 +1,27 @@
 'use strict';
 var WIZARD_NUMBER = 4;
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
-var getRandomElement = function (values) {
-  return values[Math.floor(Math.random() * values.length)];
+var getRandomNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 };
 
-var createWizard = function (wizardNames, wizardSurnames, wizardCoats, wizardEyes) {
-  var newWizard = {};
-
-  newWizard.name = getRandomElement(wizardNames) + ' ' + getRandomElement(wizardSurnames);
-  newWizard.coatColor = getRandomElement(wizardCoats);
-  newWizard.eyesColor = getRandomElement(wizardEyes);
-
-  return newWizard;
+var createWizard = function () {
+  return {
+    name: NAMES[getRandomNumber(0, NAMES.length)] + ' ' + SURNAMES[getRandomNumber(0, SURNAMES.length)],
+    coatColor: COAT_COLORS[getRandomNumber(0, COAT_COLORS.length)],
+    eyesColor: EYE_COLORS[getRandomNumber(0, EYE_COLORS.length)]
+  };
 };
 
 var createWizardList = function () {
   var wizardList = [];
 
   for (var i = 0; i < WIZARD_NUMBER; i++) {
-    var wizard = createWizard(NAMES, SURNAMES, COAT_COLORS, EYE_COLORS);
-    wizardList.push(wizard);
+    wizardList.push(createWizard());
   }
 
   return wizardList;
@@ -47,10 +48,6 @@ var putWizards = function (allWizards) {
 };
 
 var playerSettings = document.querySelector('.setup');
-var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYE_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var similarWizardsList = document.querySelector('.setup-similar-list');
 var similarWizards = document.querySelector('.setup-similar');
